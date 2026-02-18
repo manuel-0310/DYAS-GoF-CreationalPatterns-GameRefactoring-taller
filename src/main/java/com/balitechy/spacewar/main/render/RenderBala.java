a4d595861870f668cb390fc127b1927359c2bca4
manuel-0310 <manucastil0310@gmail.com>
2026-02-17 19:10:17 -0500

    Revert "first commit"
    This reverts commit a06e119d8267b58ec6fe6f7e84bc2342eec853f1.

--- src/main/java/com/balitechy/spacewar/main/render/RenderBala.java
+++
@@ -1,9 +0,0 @@
-/* Interfaz para renderizar las balas en el lienzo. */
-package com.balitechy.spacewar.main.render;
-
-import java.awt.Graphics;
-
-public interface RenderBala {
-
-    void render(Graphics g, double x, double y);
-}
--- src/main/java/com/balitechy/spacewar/main/render/RenderJugador.java
+++
@@ -1,9 +0,0 @@
-/* Interfaz para renderizar al jugador en pantalla. */
-package com.balitechy.spacewar.main.render;
-
-import java.awt.Graphics;
-
-public interface RenderJugador {
-
-    void render(Graphics g, double x, double y);
-}
